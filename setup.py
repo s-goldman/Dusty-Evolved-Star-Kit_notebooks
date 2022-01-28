db1915f563ae6615383ef2478f461ede05113926
Steve Goldman <32876747+s-goldman@users.noreply.github.com>
2022-01-28 11:30:44 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='Dusty-Evolved-Star-Kit_notebooks',
+      version='1.0',
+      description='Dusty-Evolved-Star-Kit interactive notebooks',
+      author='Steve Goldman',
+      author_email='sgoldman@usra.edu',
+      url='https://s-goldman.github.io//',
+      install_requires=[
+          "astropy",
+          "numpy",
+          "matplotlib",
+          "ipywidgets",
+          "IPython",
+      ],
+      license="BSD license",
+     )
